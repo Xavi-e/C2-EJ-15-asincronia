c7a66d26967bfda1a965b18594acfe7f31046aa1
Xavi-e <xesgawork@gmail.com>
2021-06-08 01:43:30 +0200

    iniciando apuntes al DOM y func vaciarperson

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+import { getPersonajes } from "./funciones.js";
+import { mataPersonajes } from "./funciones.js";
+import { personajes } from "./personajes.js";
+
+// APUNTANDO A ELEMENTOS DEL DOM
+const bttnCargarPersonajes = document.querySelector(".cargar-personajes");
+const bttnMatarFamilia = document.querySelector(".matar-familia");
+const buscarfamilia = document.querySelector(".familia");
+const listaPersonajes = document.querySelector(".personajes");
+const personajeDummy = document.querySelector(".personaje-dummy");
+const mensajeError = document.querySelector(".mensaje");
+
+const vaciarPersonajes = () => {
+    for (const lista of listaPersonajes.querySelector(".personajes"))
+    lista.remove();
+}
+
 
